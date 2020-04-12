0dfbd6f52918500aa074e21383a979f90c671579
Sara Bejarano <sm.bejarano44@uniandes.edu.co>
2020-04-12 16:20:27 -0500

    logeo y registro de nutricionista

--- back/routes/users.js
+++ back/routes/users.js
@@ -5,15 +5,23 @@ var db = require("../db/MongoUtils")
 router.get("/", (req, res) => res.send('Hello World!'));
 
 // Register a new user
-router.post("/new",(req,res) =>{
-    db.insertOne((user)=>{
-        res.send(user)},'users',req.body.user)
+router.post("/new", (req, res) => {
+    usuario = req.body.user
+    db.insertOne((user) => {
+        res.send(user)
+    }, 'users', usuario)
+
 });
 
 // Get user
-router.post("/", (req,res)=>{
-    db.findOne(req.body.user, 'users' , (user) => res.send(user))
+router.post("/profile", (req, res) => {
+    db.findOne((user) => res.send(user), 'users',req.body.user)
 });
+// Get users
+router.post("/", (req, res) => {
+    db.findMany( (user) => res.send(user),'users',req.body.user)
+});
+
 
 
 
