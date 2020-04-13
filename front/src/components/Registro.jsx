7dbed1557192cbbd12906b02bbebb20673bac887
marianars013 <m.rodriguez21@uniandes.edu.co>
2020-04-13 01:00:25 -0500

    Minor changes

--- front/src/components/Registro.jsx
+++ front/src/components/Registro.jsx
@@ -1,39 +1,35 @@
 import React, { useState } from "react";
-import {  Nav, Card, Container } from 'react-bootstrap';
-import RegistroPaciente from './RegistroPaciente';
-import RegistroNutri from './RegistroNutri';
-function Registro(props){
+import { Nav, Card, Container } from "react-bootstrap";
+import RegistroPaciente from "./RegistroPaciente";
+import RegistroNutri from "./RegistroNutri";
 
-    const [isPaciente,setPaciente] = useState(true)
+function Registro(props) {
+  const [isPaciente, setPaciente] = useState(true);
 
+  return (
+    <Container id="main-card">
+      <Card>
+        <Card.Header>
+          <Nav variant="tabs" defaultActiveKey="#first">
+            <Nav.Item id="button-paciente" onClick={() => setPaciente(true)}>
+              <Nav.Link href="#first">Paciente</Nav.Link>
+            </Nav.Item>
+            <Nav.Item id="button-Nutri" onClick={() => setPaciente(false)}>
+              <Nav.Link href="#second">Nutricionista</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Card.Header>
 
-    
-
-    return (
-        <Container id="main-card">
-            <Card>
-                <Card.Header>
-
-                    <Nav variant="tabs" defaultActiveKey="#first">
-
-                        <Nav.Item id="button-paciente" onClick={ () => setPaciente(true) }>
-                            <Nav.Link href="#first">Paciente</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item id="button-Nutri" onClick={ () => setPaciente(false) }>
-                            <Nav.Link href="#second">Nutricionista</Nav.Link>
-                        </Nav.Item>
-
-                    </Nav>
-                </Card.Header>
-
-                <Card.Body>
-                    { (isPaciente) ? <RegistroPaciente setUser={props.setUser}/>:<RegistroNutri setUser={props.setUser}/>  }
-                </Card.Body>
-            </Card>
-        </Container>
-    );
-
-
+        <Card.Body>
+          {isPaciente ? (
+            <RegistroPaciente setUser={props.setUser} />
+          ) : (
+            <RegistroNutri setUser={props.setUser} />
+          )}
+        </Card.Body>
+      </Card>
+    </Container>
+  );
 }
 
 export default Registro;
--- front/src/components/RegistroNutri.jsx
+++ front/src/components/RegistroNutri.jsx
@@ -1,83 +1,97 @@
-import React, { useState,  } from "react";
-import { Col, Form,Button } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Col, Form, Button } from "react-bootstrap";
 
 //Método para hacer post con fetch
-async function postData(url = '', data = {}) {
+async function postData(url = "", data = {}) {
   // Default options are marked with *
   const response = await fetch(url, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
       // 'Content-Type': 'application/x-www-form-urlencoded',
     },
-    redirect: 'follow', // manual, *follow, error
-    referrerPolicy: 'no-referrer', // no-referrer, *client
-    body: JSON.stringify(data) // body data type must match "Content-Type" header
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *client
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
   });
   return response.json(); // parses JSON response into native JavaScript objects
 }
 
 function RegistroNutri(props) {
+  const [username, setUsername] = useState("");
+  const [nombre, setNombre] = useState("");
 
+  const [apellido, setApellido] = useState("");
 
-  const [username, setUsername] = useState("")
-  const [nombre, setNombre] = useState("")
-
-  const [apellido, setApellido] = useState("")
-
-  const [password, setPassword] = useState("")
+  const [password, setPassword] = useState("");
 
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     let nutri = {
-      user: { username, nombre, apellido, password, nutri: true }
+      user: { username, nombre, apellido, password, nutri: true },
     };
     console.log(nutri);
-    postData('http://localhost:3000/user/new', nutri)
-      .then((data) => {
-        if (data.error) {
-          //avisar al usuario del error
-        }
-        else {
-          nutri = { username, password, nutri: true };
-          postData('http://localhost:3000/login', nutri)
-            .then((data) => props.setUser(data.user));
-        }
-      })
+    postData("http://localhost:3000/user/new", nutri).then((data) => {
+      if (data.error) {
+        //avisar al usuario del error
+      } else {
+        nutri = { username, password, nutri: true };
+        postData("http://localhost:3000/login", nutri).then((data) =>
+          props.setUser(data.user)
+        );
+      }
+    });
   };
 
   return (
-    <Form  onSubmit={handleSubmit}>
+    <Form onSubmit={handleSubmit}>
       <Form.Row>
         <Form.Group as={Col} controlId="formGridUsername">
           <Form.Label>Email</Form.Label>
-          <Form.Control required type="email" placeholder="Username" onChange={e => setUsername(e.target.value)} />
+          <Form.Control
+            required
+            type="email"
+            placeholder="Username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </Form.Group>
 
         <Form.Group as={Col} controlId="formGridPassword">
           <Form.Label>Contraseña</Form.Label>
-          <Form.Control required type="password" placeholder="Contraseña" onChange={e => setPassword(e.target.value)} />
+          <Form.Control
+            required
+            type="password"
+            placeholder="Contraseña"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </Form.Group>
-
       </Form.Row>
       <Form.Row>
         <Form.Group as={Col} controlId="formGridNombre">
           <Form.Label>Nombres</Form.Label>
-          <Form.Control required type="text" placeholder="Nombres" onChange={e => setNombre(e.target.value)} />
+          <Form.Control
+            required
+            type="text"
+            placeholder="Nombres"
+            onChange={(e) => setNombre(e.target.value)}
+          />
         </Form.Group>
         <Form.Group as={Col} controlId="formGridApellido">
           <Form.Label>Apellidos</Form.Label>
-          <Form.Control required type="text" placeholder="Apellidos" onChange={e => setApellido(e.target.value)} />
+          <Form.Control
+            required
+            type="text"
+            placeholder="Apellidos"
+            onChange={(e) => setApellido(e.target.value)}
+          />
         </Form.Group>
-
-
       </Form.Row>
       <Button variant="primary" type="submit">
         Registrarme
-    </Button>
+      </Button>
     </Form>
   );
 }
