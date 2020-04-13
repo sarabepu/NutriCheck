8d8d993aa467903328df18e1bf94066423839b35
marianars013 <m.rodriguez21@uniandes.edu.co>
2020-04-13 01:16:16 -0500

    Layout changes

--- front/src/components/Datos.jsx
+++ front/src/components/Datos.jsx
@@ -7,9 +7,15 @@ import Medidas from "./Medidas";
 function Datos(props) {
   return (
     <Container>
+      <h1 className="white-text mt-2" align="center">
+        Mis datos ({props.user.username})
+      </h1>
       <Row>
-
-        <Preferencias as={Col} user={props.user} setUser={props.setUser}></Preferencias>
+        <Preferencias
+          as={Col}
+          user={props.user}
+          setUser={props.setUser}
+        ></Preferencias>
         <Medidas as={Col} user={props.user} />
       </Row>
     </Container>
