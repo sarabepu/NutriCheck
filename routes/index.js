b75e88542d277d385a2678c6e88b347ae9eac31f
marianars013 <m.rodriguez21@uniandes.edu.co>
2020-04-13 12:34:00 -0500

    Static

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+/* GET home page. */
+router.get("/", function (req, res) {
+  res.render("index", { user: req.user });
+});
+
 module.exports = router;
